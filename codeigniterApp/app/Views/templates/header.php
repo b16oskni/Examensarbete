47f0568f809b5c9af1bda5a803508ca9c63214ad
b16oskni <62884511+b16oskni@users.noreply.github.com>
2022-04-17 20:38:57 +0200

    Cleaning code
    Cleaning code and preparing for changing to one static page for CI/Symfony each

--- codeigniterApp/app/Views/templates/header.php
+++ codeigniterApp/app/Views/templates/header.php
@@ -7,27 +7,45 @@
     <!-- Bootstrap -->
     <link href="<?php echo base_url('bootstrap/css/bootstrap.min.css');?>" rel="stylesheet">
     <script src="<?php echo base_url('bootstrap/js/bootstrap.bundle.min.js');?>"></script>
+    <style>
+        html, body {
+            height: 100vh;
+        }
+        body {
+            background-color: #f5f4fa;
+        }
+        .cbtn {
+            width: 200px;
+            height: 110px;
+            border-radius: 10px;
+        }
+        p {
+            font-family: 'Franklin Gothic Medium';
+            font-size: 1.3rem;
+        }
+        h1 {
+            font-size: 3rem;
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
     <header class="p-3 bg-dark text-white">
         <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-
-            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="<?php echo base_url('public/home')?>" class="nav-link px-2 text-white">Home</a></li>
-            <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
-            <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-            <li><a href="#" class="nav-link px-2 text-white">About</a></li>
-            </ul>
-
-            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-            <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
-            </form>
-
-            <div class="text-end">
-            <button type="button" class="btn btn-outline-light me-2">Login</button>
-            <button type="button" class="btn btn-warning">Sign-up</button>
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="<?php echo base_url('public/home')?>" class="nav-link px-2 text-white">Home</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+                </ul>
+                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                    <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+                </form>
+                <div class="text-end">
+                    <button type="button" class="btn btn-outline-light me-2">Login</button>
+                    <button type="button" class="btn btn-warning">Sign-up</button>
+                </div>
             </div>
-        </div>
         </div>
     </header>
